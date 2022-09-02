d769db3e6d8918230d2ef40a84f9f7fce68bec39
happyhermann <carrick035@gmail.com>
2022-09-02 13:59:45 +0900

    Implement HomeScreen
    Banner using help function 'makeImagePath' Styled component props
    
    this function is helping sepecific movie's image ID url return

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,3 +1,62 @@
+import React from "react";
+import { useQuery } from "react-query";
+import styled from "styled-components";
+import { getMovies, IGetMoviesResult } from "../api";
+import { makeImagePath } from "./utils";
+
+const Wrapper = styled.div`
+  background: black;
+`;
+
+const Loader = styled.div`
+  height: 20vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Banner = styled.div<{ bgPhoto: string }>`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 60px;
+  background-image: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 1)),
+    url(${(props) => props.bgPhoto});
+  background-size: cover;
+`;
+
+const Title = styled.h2`
+  font-size: 68px;
+  margin-bottom: 20px; ;
+`;
+
+const Overview = styled.p`
+  font-size: 30px;
+  width: 50%;
+`;
+
 export default function Home() {
-  return <div style={{ height: "200vh" }}>123</div>;
+  const { data, isLoading } = useQuery<IGetMoviesResult>(
+    ["movies", "nowPlaying"],
+    getMovies
+  );
+  // 리액트 쿼리, 비동기 로직을 리액트스럽게 다룰 수 있게 해줌
+  // isLoading, isError, refetch, 데이터 캐싱을 제공해줌
+
+  return (
+    <Wrapper style={{ height: "200vh" }}>
+      {isLoading ? (
+        <Loader>Loading...</Loader>
+      ) : (
+        <>
+          <Banner bgPhoto={makeImagePath(data?.results[0].backdrop_path || "")}>
+            <Title>{data?.results[0].title}</Title>
+            <Overview>{data?.results[0].overview}</Overview>
+          </Banner>
+        </>
+        // Fragment: 많은 요소를 부모 없이 연이어서 리턴할 수 있는 방법
+      )}
+    </Wrapper>
+  );
 }
