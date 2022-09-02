403bd361be9361ce23da86223ba65abd1378d926
happyhermann <carrick035@gmail.com>
2022-09-02 12:57:32 +0900

    Add Movie Api with Fecther

---
+++ src/api.ts
@@ -0,0 +1,27 @@
+const API_KEY = "54ced1bcd128a05a9903a6fc97efa16e";
+const BASE_PATH = "https://api.themoviedb.org/3";
+
+interface IMovie {
+  id: number;
+  backdrop_path: string;
+  poster_path: string;
+  title: string;
+  overview: string;
+}
+
+export interface IGetMoviesResult {
+  dates: {
+    maximum: string;
+    minimun: string;
+  };
+  page: number;
+  result: IMovie[];
+  total_pages: number;
+  total_result: number;
+}
+
+export function getMovies() {
+  return fetch(`${BASE_PATH}/movie/now_playing?api_key=${API_KEY}`).then(
+    (response) => response.json()
+  );
+}
