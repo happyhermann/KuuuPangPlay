a156c1bbe12b6c3d604f2aec742baf5691c7a25b
happyhermann <carrick035@gmail.com>
2022-09-05 16:13:38 +0900

    Implement Slider and Pagination

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import { useQuery } from "react-query";
 import styled from "styled-components";
+import { motion, AnimatePresence } from "framer-motion";
 import { getMovies, IGetMoviesResult } from "../api";
 import { makeImagePath } from "./utils";
 
@@ -36,6 +37,45 @@ const Overview = styled.p`
   width: 50%;
 `;
 
+const Slider = styled.div`
+  position: relative;
+  top: -100px;
+`;
+
+const Row = styled(motion.div)`
+  display: grid;
+  gap: 10px;
+  grid-template-columns: repeat(6, 1fr);
+  position: absolute;
+  width: 100%;
+`;
+
+const Box = styled(motion.div)<{ bgPhoto: string }>`
+  background-color: white;
+  background-image: url(${(props) => props.bgPhoto});
+  background-size: cover;
+  background-position: center center;
+  height: 200px;
+  font-size: 66px;
+`;
+
+const rowVariants = {
+  hidden: {
+    x: window.innerWidth - 10,
+  },
+  visible: {
+    x: 0,
+  },
+  exit: {
+    x: -window.innerWidth - 10,
+  },
+};
+
+// Pagination
+
+const offset = 6;
+// 한 번에 보여주고 싶은 영화 수
+
 export default function Home() {
   const { data, isLoading } = useQuery<IGetMoviesResult>(
     ["movies", "nowPlaying"],
@@ -44,16 +84,60 @@ export default function Home() {
   // 리액트 쿼리, 비동기 로직을 리액트스럽게 다룰 수 있게 해줌
   // isLoading, isError, refetch, 데이터 캐싱을 제공해줌
 
+  const [index, setIndex] = useState(0);
+  const [leaving, setLeaving] = useState(false);
+
+  const increaseIndex = () => {
+    if (data) {
+      if (leaving) return;
+      // leaving이 true면 아무것도 하지 않을 것
+      toggleLeaving();
+      const totalMovies = data?.results.length - 1;
+      const maxIndex = Math.floor(totalMovies / offset) - 1;
+      setIndex((prev) => (prev === maxIndex ? 0 : prev + 1));
+    }
+  };
+
+  const toggleLeaving = () => setLeaving((prev) => !prev);
+
   return (
     <Wrapper style={{ height: "200vh" }}>
       {isLoading ? (
         <Loader>Loading...</Loader>
       ) : (
         <>
-          <Banner bgPhoto={makeImagePath(data?.results[0].backdrop_path || "")}>
+          <Banner
+            onClick={increaseIndex}
+            bgPhoto={makeImagePath(data?.results[0].backdrop_path || "")}
+          >
             <Title>{data?.results[0].title}</Title>
             <Overview>{data?.results[0].overview}</Overview>
           </Banner>
+          <Slider>
+            <AnimatePresence initial={false} onExitComplete={toggleLeaving}>
+              {/*
+              initial props : 컴포넌트가 처음 렌더링될 때 자식의 초기 애니메이션을 비활성화합니다.
+              on ExitComplete props :exit 중인 모든 노드들이 애니메이션을 끝내면 실행됩니다. */}
+              <Row
+                variants={rowVariants}
+                initial="hidden"
+                animate="visible"
+                exit="exit"
+                transition={{ type: "tween", duration: 1 }}
+                key={index}
+              >
+                {data?.results
+                  .slice(1)
+                  .slice(offset * index, offset * index + offset)
+                  .map((movie) => (
+                    <Box
+                      key={movie.id}
+                      bgPhoto={makeImagePath(movie.backdrop_path, "w500")}
+                    />
+                  ))}
+              </Row>
+            </AnimatePresence>
+          </Slider>
         </>
         // Fragment: 많은 요소를 부모 없이 연이어서 리턴할 수 있는 방법
       )}
